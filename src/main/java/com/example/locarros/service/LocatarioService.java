5f3f7ad745b8614c704a62bac29e976ddd6ff319
Duuduaraujo <araujoe469@gmail.com>
2024-11-11 16:01:05 -0300

    primeiro commit

---
+++ src/main/java/com/example/locarros/service/LocatarioService.java
@@ -0,0 +1,12 @@
+package com.example.locarros.service;
+
+import com.example.locarros.repository.LocatarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class LocatarioService {
+
+    @Autowired
+    private LocatarioRepository locatarioRepository;
+
+
+}
