326ba468edd1e6e788485675d7c532ca3cf80801
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-11-26 14:32:33 -0300

    fix: removendo comentários

--- src/main/java/com/example/locarros/repository/AluguelRepository.java
+++ src/main/java/com/example/locarros/repository/AluguelRepository.java
@@ -8,12 +8,9 @@ import java.util.List;
 
 @Repository
 public interface AluguelRepository extends JpaRepository<Aluguel, Integer> {
-    // Buscar aluguéis por locador
     List<Aluguel> findByLocador_Id(int locadorId);
 
-    // Buscar aluguéis por locatário
     List<Aluguel> findByLocatario_Id(int locatarioId);
 
-    // Buscar aluguéis por carro
     List<Aluguel> findByCarro_Id(int carroId);
 }
